13c068b3bd4c8d59b836294600ccda4dd1f3c634
junilhwang <junil.h@kakao.com>
2025-09-26 10:27:07 +0900

    feat: 솔루션코드 추가

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
-    poolOptions: {
-      threads: {
-        singleThread: true,
+const base = process.env.NODE_ENV === "production" ? "/front_lite_chapter2-1/" : "";
+
+export default mergeConfig(
+  defineConfig({
+    base,
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          404: resolve(__dirname, "404.html"),
+        },
       },
     },
-  },
-});
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js",
+      exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+      poolOptions: {
+        threads: {
+          singleThread: true,
+        },
+      },
+    },
+  }),
+);
